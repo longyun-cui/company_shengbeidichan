cbe2a4389f5dd31a37ff4b926a5885eec9f37dff
longyun <longyun@longyunMacBook-Pro.local>
2018-11-12 13:28:53 +0800

    beian

--- resources/views/frontend/component/footer.blade.php
+++ resources/views/frontend/component/footer.blade.php
@@ -41,11 +41,15 @@
             </div>
         </div>
     </div>
-    <div class="site-footer-bottom _none">
+    <div class="site-footer-bottom">
         <div class="container">
-{{--            <p class="copyright pull-left wow slideInRight">Copyright © 2018.{{ config('company、.info.name') }} All rights reserved.</p>--}}
-            <p class="copyright pull-left wow slideInRight">Copyright © 2018.寻觅e家 All rights reserved.</p>
-            <nav class="footer-nav pull-right wow slideInLeft">
+            {{--<p class="copyright pull-left wow slideInRight">Copyright © 2018.{{ config('company、.info.name') }} All rights reserved.</p>--}}
+            {{--<p class="copyright pull-left wow slideInRight">Copyright © 2018.上海盛贝房地产营销策划有限公司 All rights reserved.</p>--}}
+            <p class="copyright pull-left wow slideInRight">
+                版权所有@上海盛贝房地产营销策划有限公司(2018) &nbsp;&nbsp;
+                <a href="http://www.miitbeian.gov.cn" target="_blank">备案号:沪ICP备18041650号</a>
+            </p>
+            <nav class="footer-nav pull-right wow slideInLeft _none">
                 <ul>
                     <li><a href="{{ url('/houses') }}">楼盘</a></li>
                     <li><a href="{{ url('/contact') }}">联系我们</a></li>
