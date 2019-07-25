a7d3ecad6191e593a01af3b82ba4f19ed52e4249
Lonyun Cui <longyun-cui@163.com>
2019-07-25 18:43:01 +0800

    text

--- resources/views/frontend/custom/component/footer.blade.php
+++ resources/views/frontend/custom/component/footer.blade.php
@@ -6,7 +6,8 @@
             {{--<p class="copyright pull-left wow slideInRight">Copyright © 2018.上海盛贝房地产营销策划有限公司 All rights reserved.</p>--}}
             <p class="copyright pull-left wow slideInRight">
                 版权所有@上海盛贝房地产营销策划有限公司(2018) &nbsp;&nbsp;
-                <a href="http://www.miitbeian.gov.cn" target="_blank">备案号:沪ICP备18041650号</a>
+                <a href="http://www.miitbeian.gov.cn" target="_blank">备案号:沪ICP备18041650号</a> &nbsp;&nbsp;
+                云麓里 授权上海盛贝房地产营销策划有限公司进行百度广告投放
             </p>
             <nav class="footer-nav pull-right wow slideInLeft _none">
                 <ul>
